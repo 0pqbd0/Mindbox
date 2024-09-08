899d9609884761ad5630b79cd6911fa082a92437
Artem Mikulskii <st107472@student.spbu.ru>
2024-09-08 11:37:28 +0300

    Add Circle

---
+++ FigureArea/Circle.cs
@@ -0,0 +1,20 @@
+﻿namespace FigureArea;
+
+public class Circle : IFigure
+{
+    public double Radius { get; }
+
+    public Circle(double radius)
+    {
+        if (radius <= 0)
+        {
+            throw new ArgumentException("Радиус должен быть положительным!");
+        }
+        Radius = radius;
+    }
+
+    public double GetArea()
+    {
+        return Math.PI * Radius * Radius;
+    }
+}
